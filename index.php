28a2ce60607fb4395f234482f1d93f8c5971d9db
valentin-49 <55395531+valentin-49@users.noreply.github.com>
2020-11-02 17:40:20 +0100

    lol

--- index.php
+++ index.php
@@ -47,65 +47,26 @@ session_start();
     session_unset();
     session_destroy();
 
-}  if(isset($_POST['deco3'])){ //bouton de deconnexion qui retourne à la condition 2
-    session_unset();
-    session_destroy();
-
-} 
+}  
 if(isset($_SESSION["isconnectUS"]) && $_SESSION["isconnectUS"]==true && $_SESSION["isconnectAD"]==false ){ //condition 1) si l'utilisateur est deja connecter et n'est pas un admin, affiche les redirections vers d'autres pages.
 ?>
  <html>
-    <head><!-- fichier css et js de mise en page-->
-        <title>GeoGuessBoat</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="bootstrap-4.4.1-dist/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="index.css">
-        <link rel="shortcut icon" href="image/unnamed.png" />
-        <script type="text/javascript" src="index.js"></script>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-                    
-    <body onload="heure()">
-            <div class="row">
-                <div class="col-2"></div>
-                <div class="col-8 en-tete" align="center">
-                    <h1>Vous etes connectez</h1>
-                </div>
-                <div class="col-2"></div>
-                <div class="col-4" align="center"></div>
-                <div class="col-4 en-tete " align="center">
-                    <h2 id="time" style="text-decoration: underline;"></h2>
-                </div>
-                <div class="col-4" align="center"></div>
+
+    <body>
+        <div class="hero">
+            <div class="form-box2">
+               <form id="login" class="input-group2" method="POST"> 
+                    <input type="button" name="Valider" value="Acces a mon compte" onclick="self.location.href='acces_mon_compte/compte.php'" class="submit-btn2"/> 
+                    <input type="button" name="Valider" value="Acces aux donnees" onclick="self.location.href='acces_donnee/data.php'" class="submit-btn2"/> 
+                    <input type="button" name="Valider" value="Position des bateaux" onclick="self.location.href='position_bateaux/traceur.php'" class="submit-btn2"/> 
+                    <input type="submit" name="deco" value="deconnexion" class="submit-btn2"/>
+                </form>   
             </div>
-        <div class="row">
-            <div class="col-12"></div>
-            <div class="col-2 " align="center"></div>
-            <div class="col-8" align="center">
-        <?php
-                    
-            if($_SESSION["isconnectUS"]){
-        ?>
-            <div class="col-12 redirection" align="center">
-                
-                <form action="index.php" method="POST">
-                    <div class="row">
-                        <div class="col-4" align="center"><p><input type="button" name="Valider" value="Accés à mon compte" onclick="self.location.href='acces_mon_compte/compte.php'" class="bouton_redirect"/></p></div>                                      
-                        <div class="col-4" align="center"><p><input type="button" name="Valider" value="Accés aux donnée" onclick=" self.location.href='acces_donnee/data.php'" class="bouton_redirect"/></p></div>
-                        <div class="col-4" align="center"> <p><input type="button" name="Valider" value="Position des bateaux" onclick="self.location.href='position_bateaux/traceur.php'" class="bouton_redirect"/></p></div>
-                    </div>
-                        <div class="col-12" align="center"><form method="POST"><p><input type="submit" name="deco2" value="deconnexion" class="bouton_redirect"/></p></form>
-                    </div>
-                </form>
-            </div>
-        <?php      
-        }
-        ?>
+            
         </div>
-            <div class="col-2 " align="center">
-                                                
-                    </div>
-                </div>
-            </div>
     </body>
 </html>
 <?php 
@@ -114,127 +75,77 @@ if(isset($_SESSION["isconnectUS"]) && $_SESSION["isconnectUS"]==true && $_SESSIO
 
 ?>
 <html>
-    <head><!-- fichier css et js de mise en page-->
-        <title>GeoGuessBoat</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="bootstrap-4.4.1-dist/css/bootstrap.css"> 
-        <link rel="stylesheet" type="text/css" href="index.css">
-        <link rel="shortcut icon" href="image/unnamed.png" />
-        <script type="text/javascript" src="index.js"></script>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
 
-    <body onload="heure()">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 en-tete" align="center">
-                        <h1>GeoGuessBoat</h1>
-                    </div>
-                    <div class="col-4" align="center">
-                    </div>
-                    <div class="col-4 en-tete " align="center">
-                        <h2 id="time" style="text-decoration: underline;"></h2>
-                    </div>
-                    <div class="col-4" align="center"></div>
-                    <div class="col-5 en-tete" align="center">
-                        <form method="POST"><input type="submit" name="connect" value="Me connecter" class="bouton"/></form>
-                    </div>
-                    <div class="col-2"></div>
-                    <div class="col-5 en-tete" align="center">
-                        <form method="POST"><input type="submit" name="insert" value="M'inscrire" class="bouton"/></form>
-                    </div> 
-                </div> 
-                <?php
-                    if(isset($_POST['connect'])){ //affiche le formulaire de connexion
-                    ?>
-                        <div class="col-12 formulaire_de_connexion" align="center">
-                        <form action="index.php" method="POST">
-                            <label><h3>Se Connecter</h3></label>
-                            <p><input type="text" value="" name="ID_1" width="auto" class="text" required/></p>
-                            <label><h3>Mot De Passe</h3></label>
-                            <p><input type="password" value="" name="MDP_1" class="text" required/></p>
-                            <p><input type="submit" name="Valider" value="Valider" class="bouton"/></p>
-                        </form>
-                    </div>
-                <?php 
-                    }
-                    elseif(isset($_POST['insert'])){ //affiche le formulaire d'inscription
-                ?> 
-                    <div class="col-12 formulaire_de_connexion" align="center">
-                    <h1>S'inscrire</h1>
-                        <form action="index.php" method="POST">
-                            <label><h3>pseudo</h3></label>
-                            <p><input type="text" value="" name="new_ID" width="auto" class="text" required/></p>
-                            <label><h3>Mot De Passe</h3></label>
-                            <p><input type="password" value="" name="new_MDP" class="text" required/></p>
-                            <p><input type="submit" name="Valider" value="Valider" class="bouton"/></p>
-                        </form>
-                    
-                    </div>
-                <?php 
-                    }
-                ?> 
-                <div class="row">
-                    <div class="col-12 image" align="center">
-                    </div>  
-                </div>
-            </div>  
+    <body>
+        <div class="background">
+            <div class="form-box">
+               <div class="button-box">
+                   <div id="btn"></div>
+                   <button type="button" class="toggle-btn" onclick="login()">Se Connecter</button>
+                   <button type="button" class="toggle-btn" onclick="register()">S'inscrire</button>
+               </div>
+               <div class="social">
+                    <img src="image/fb.png">
+                    <img src="image/tw.png">
+                    <img src="image/gp.png">
+               </div>
+               <form id="login" class="input-group" method="POST">
+                    <input type="text" class="input-field" placeholder="Pseudo" name="ID_1" required>
+                    <input type="text" class="input-field" placeholder="Mot de passe" name="MDP_1" required>
+                    <button type="submit" class="submit-btn">Se Connecter</button>
+                </form>
+                <form id="register" class="input-group" method="POST">
+                    <input type="text" class="input-field" placeholder="Pseudo" name="new_ID" required>
+                    <input type="email" class="input-field" placeholder="E-mail" name="new_mail" required>
+                    <input type="text" class="input-field" placeholder="Mot de passe" name="new_MDP" required>
+                    <button type="submit" class="submit-btn">S'inscrire</button>
+                </form>
+            </div>
+        </div>
+        <script>
+
+            var x = document.getElementById("login");
+            var y = document.getElementById("register");
+            var z = document.getElementById("btn");
+
+            function register(){
+                x.style.left = "-400px";
+                y.style.left = "50px";
+                z.style.left = "140px";
+            }
+
+            function login(){
+                x.style.left = "50px";
+                y.style.left = "450px";
+                z.style.left = "0px";
+            }
+
+        </script>
     </body>
 </html>
 <?php
     }elseif(isset($_SESSION["isconnectAD"]) || $_SESSION["isconnectAD"]==true ){ //condition 3) si l'utilisateur est connecter ete et un admin, affiche la redirection vers la page d'administration
 ?>
 <html>
-    <head><!-- fichier css et js de mise en page-->
-        <title>GeoGuessBoat</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="bootstrap-4.4.1-dist/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="index.css">
-        <link rel="shortcut icon" href="image/unnamed.png" />
-        <script type="text/javascript" src="index.js"></script>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-                    
-    <body onload="heure()">
-            <div class="row">
-                <div class="col-2"></div>
-                <div class="col-8 en-tete" align="center">
-                    <h1>Vous etes connectez</h1>
-                </div>
-                <div class="col-2"></div>
-                <div class="col-4" align="center"></div>
-                <div class="col-4 en-tete " align="center">
-                    <h2 id="time" style="text-decoration: underline;"></h2>
-                </div>
-                <div class="col-4" align="center"></div>
-            </div>
-        <div class="row">
-            <div class="col-12"></div>
-            <div class="col-2 " align="center"></div>
-            <div class="col-8" align="center">
-        <?php
-                    
-            if($_SESSION["isconnectAD"]){
-        ?>
-            <div class="col-12 redirection" align="center">
-                
-                <form action="index.php" method="POST">
-                    <div class="row">
-                        <div class="col-6" align="center"><p><input type="button" name="Valider" value="Accés à mon compte" onclick="self.location.href='acces_mon_compte/compte.php'" class="bouton_redirect"/></p></div>
-                        <div class="col-6" align="center"><p><input type="button" name="Valider" value="Accées administrateur" onclick="self.location.href='acces_admin/admin.php'" class="bouton_redirect"/></p></div>
-                    </div>
-                        <div class="col-12" align="center"><form method="POST"><p><input type="submit" name="deco2" value="deconnexion" class="bouton_redirect"/></p></form>
-                    
-                    </div>
-                </form>
-            </div>
-        <?php      
-        }
-        ?>
+
+    <body>
+        <div class="hero">
+            <div class="form-box2">
+               <form id="login" class="input-group2" method="POST">
+                    <input type="button" name="Valider" value="admin" onclick="admin()" class="submit-btn2"/> 
+                    <input type="button" name="Valider" value="Acces a mon compte" onclick="compte()" class="submit-btn2"/> 
+                    <input type="button" name="Valider" value="Acces aux donnees" onclick="data()" class="submit-btn2"/> 
+                    <input type="button" name="Valider" value="Position des bateaux" onclick="position()" class="submit-btn2"/> 
+                    <input type="submit" name="deco2" value="deconnexion" class="submit-btn2"/>
+                </form> 
+            </div> 
         </div>
-            <div class="col-2 " align="center">
-                                                
-                    </div>
-                </div>
-            </div>
     </body>
 </html>
 
